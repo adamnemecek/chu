f9f58e25aefad5644dafc415eb49bbeff0acd0c9
adamnemecek <adamnemecek@gmail.com>
2023-02-13 10:34:26 -0800

    stuff

--- src/chu.rs
+++ src/chu.rs
@@ -6,6 +6,10 @@ pub struct Matrix<T: Copy + Default> {
     data: Vec<T>,
 }
 
+fn t<T>(a: (T, T))-> (T, T){
+    (a.1, a.0)
+}
+
 impl<T: Copy + Default> Matrix<T> {
     pub fn new(shape: (usize, usize)) -> Self {
         Self {
@@ -16,6 +20,11 @@ impl<T: Copy + Default> Matrix<T> {
 
     pub fn transpose(&self) -> Self {
         let mut ret = Self::new((self.shape.1, self.shape.0));
+        for i in 0..self.shape.0{
+            for j in 0..self.shape.1 {
+                ret[(j,i)] = self[(i,j)];
+            }
+        }
         ret
     }
 }
@@ -38,7 +47,7 @@ impl<T: Copy + Default> std::ops::IndexMut<(usize, usize)> for Matrix<T> {
 pub struct Chu {
     k: usize,
 }
-
+g
 impl Chu {
     pub fn new() -> Self {
         unimplemented!()
