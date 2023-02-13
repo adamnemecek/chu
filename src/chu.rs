18ab464977221d1bcbf52b095e3b1a60609f63b6
adamnemecek <adamnemecek@gmail.com>
2023-02-13 10:59:08 -0800

    stuff

--- src/chu.rs
+++ src/chu.rs
@@ -69,9 +69,7 @@ impl<T: Copy + Default> std::ops::IndexMut<(usize, usize)> for Matrix<T> {
 impl<T: Copy + Default + Debug> std::fmt::Debug for Matrix<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for row in 0..self.shape.0 {
-            let r = self.row(row);
-
-            write!(f, "{:?}", r)?;
+            writeln!(f, "{:?}", self.row(row))?;
         }
         Ok(())
     }
