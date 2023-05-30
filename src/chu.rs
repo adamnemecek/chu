e10b3209e65db5b8a106a28b93dfe397856a293e
adamnemecek <adamnemecek@gmail.com>
2023-05-30 12:24:23 -0700

    working on implication

--- src/chu.rs
+++ src/chu.rs
@@ -430,17 +430,74 @@ impl Chu {
 
     pub fn implication(&self, other: Self) -> Self {
         let k = self.k.max(other.k);
+        // let size = self.rows() * other.cols();
+
+        // let mut mg = MatrixGenerator::new(other.row_tree(), self.col_tree());
+
+        // while mg.next() {
+        //     //
+        //     let mut num_instances = 1;
+        //     // for r in mg.nrows() {
+        //     //
+        //     // }
+        // }
+        // unimplemented!()
+
         let size = self.rows() * other.cols();
+        let mut transforms: Vec<Vec<i32>> = Vec::new();
+        let MG = MatrixGenerator::new(other.row_tree(), self.col_tree());
 
-        let mut mg = MatrixGenerator::new(other.row_tree(), self.col_tree());
+        // while MG.next() {
+        //     let mut num_instances = 1;
+        //     for r in 0..MG.nrows() {
+        //         let mut l = MG.row_links[r];
+        //         let mut length = 0;
+        //         while let Some(next_l) = l.next() {
+        //             l = next_l;
+        //             length += 1;
+        //         }
+        //         num_instances *= length;
+        //     }
+        //     for c in 0..MG.ncols() {
+        //         let mut l = MG.col_links[c];
+        //         let mut length = 0;
+        //         while let Some(next_l) = l.next() {
+        //             l = next_l;
+        //             length += 1;
+        //         }
+        //         num_instances *= length;
+        //     }
 
-        while mg.next() {
-            //
-            let mut num_instances = 1;
-            // for r in mg.nrows() {
-            //
-            // }
-        }
+        //     let mut transform: Vec<i32> = vec![0; size];
+        //     for r in 0..MG.rows() {
+        //         for c in 0..MG.cols() {
+        //             let row_index = MG.row_links[r].datum();
+        //             let row = B.matrix[row_index];
+        //             let entry = row[c];
+        //             transform[r * MG.cols() + c] = entry;
+        //         }
+        //     }
+
+        //     for _ in 0..num_instances {
+        //         transforms.push(transform.clone());
+        //     }
+        // }
+
+        // let new_nrows = transforms.len();
+        // // let mut matrix: Vec<Vec<i32>> = vec![vec![0; size]; new_nrows];
+        // let mut m = Matrix::<usize>::new((new_rows, size));
+        // for (i, transform) in transforms.iter().enumerate() {
+        //     m[i] = transform.clone();
+        // }
+
+        // Chu {
+        //     K,
+        //     nrows: new_nrows,
+        //     ncols: size,
+        //     matrix,
+        //     transpose: false,
+        // }
         unimplemented!()
+        // Self::new(k, data, false)
     }
 }
